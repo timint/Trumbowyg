508ec4493df3dd2ba630b2b2c39da45b4a0aec1a
T. Almroth <info@tim-international.net>
2021-08-03 04:15:04 +0200

    * Fix duplicate banner comments in minified files

--- gulpfile.js
+++ gulpfile.js
@@ -71,7 +71,11 @@ const scripts = gulp.series(testScripts, function scripts() {
         .pipe(gulp.dest('dist/'))
         .pipe($.size({title: 'trumbowyg.js'}))
         .pipe($.rename({suffix: '.min'}))
-        .pipe($.terser())
+        .pipe($.terser({
+            format: {
+                comments: false
+            }
+        }))
         .pipe($.header(bannerLight, {pkg: pkg}))
         .pipe(gulp.dest('dist/minified/'))
         .pipe($.size({title: 'trumbowyg.min.js'}));
@@ -127,7 +131,7 @@ const styles = function () {
         .pipe(gulp.dest('dist/ui/'))
         .pipe($.size({title: 'trumbowyg.css'}))
         .pipe($.rename({suffix: '.min'}))
-        .pipe($.minifyCss())
+        .pipe($.minifyCss({keepSpecialComments : 0}))
         .pipe($.header(bannerLight, {pkg: pkg}))
         .pipe(gulp.dest('dist/minified/ui/'))
         .pipe($.size({title: 'trumbowyg.min.css'}));
@@ -149,7 +153,7 @@ const pluginsStyles = function () {
         }))
         .pipe(gulp.dest('dist/plugins/'))
         .pipe($.rename({suffix: '.min'}))
-        .pipe($.minifyCss())
+        .pipe($.minifyCss({keepSpecialComments : 0}))
         .pipe($.header(bannerLight, {pkg: pkg}))
         .pipe(gulp.dest('dist/minified/plugins/'))
         .pipe($.size({title: 'Plugins styles'}));
